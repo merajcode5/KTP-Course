796ed48d2772e8a97464a17dc1a9839de90326a4
meraj <mehrajmohd00@gmail.com>
2024-05-18 21:03:38 +0530

    StageOfLifeMiniApp

--- ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
+++ ProgrammingFundamentals/Ep2/StageOfLife/src/Task.kt
@@ -1,7 +1,16 @@
 import com.faangx.ktp.basics.StageOfLifeMiniApp
 
 fun stageOfLife(age: Int): String {
-    TODO()
+    return if (age < 3)
+        "Infancy"
+    else if (age < 12)
+            "Childhood"
+    else if (age < 18)
+                "Teenage"
+    else if (age < 60)
+                    "Adulthood"
+    else
+        "Old age"
 }
 
 fun main() {
