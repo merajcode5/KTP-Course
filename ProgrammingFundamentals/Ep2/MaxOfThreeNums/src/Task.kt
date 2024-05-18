9ae6c59ff28ac4144106de0abe79986811891c28
meraj <mehrajmohd00@gmail.com>
2024-05-18 20:55:42 +0530

    LeapYearCheckerMiniApp

--- ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
+++ ProgrammingFundamentals/Ep2/MaxOfThreeNums/src/Task.kt
@@ -1,7 +1,13 @@
 import com.faangx.ktp.basics.MaxOfThreeNumsMiniApp
 
 fun maxOf(x: Int, y: Int, z: Int): Int {
-    TODO()
+    return if (x >= y && x >= z) {
+        x
+    } else if (y >= x && y >= z) {
+        y
+    } else {
+        z
+    }
 }
 
 fun main() {
