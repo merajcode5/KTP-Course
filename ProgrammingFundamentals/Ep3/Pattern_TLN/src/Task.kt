db714451fa82ce921b7b09668af3aa0bd2e75d20
merajcode5 <mehrajmohd00@gmail.com>
2024-05-21 12:41:11 +0530

    Pattern_TLN

--- ProgrammingFundamentals/Ep3/Pattern_TLN/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_TLN/src/Task.kt
@@ -1,7 +1,10 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int) {
-    TODO()
+    repeat(lines) {i ->
+        repeat(lines - i) {print(lines - i)}
+        println()
+    }
 }
 
 fun main() {
