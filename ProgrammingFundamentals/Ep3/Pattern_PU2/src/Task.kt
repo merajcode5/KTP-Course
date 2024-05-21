98f1a7ceb0d395c1a2ce2cb46633400e424186a4
merajcode5 <mehrajmohd00@gmail.com>
2024-05-21 12:42:08 +0530

    Pattern_PU2

--- ProgrammingFundamentals/Ep3/Pattern_PU2/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_PU2/src/Task.kt
@@ -1,7 +1,26 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines) { i ->
+        // Print leading spaces
+        repeat(lines - 1 - i) { print(' ') }
+
+        // Print the first char1
+        print(char1)
+
+        // Print the middle part with char2 if not the first or last line
+        if (i != 0 && i != lines - 1) {
+            repeat(2 * i - 1) { print(char2) }
+            print(char1)  // Print the last char1 for the current line
+        }
+
+        // If it's the last line, fill with char1
+        if (i == lines - 1) {
+            repeat(2 * i) { print(char1) }
+        }
+
+        println()
+    }
 }
 
 fun main() {
