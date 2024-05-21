b732407c80b961abcc29f6f4709605fe254219e8
merajcode5 <mehrajmohd00@gmail.com>
2024-05-21 12:42:29 +0530

    Pattern_PD3

--- ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
+++ ProgrammingFundamentals/Ep3/Pattern_PD3/src/Task.kt
@@ -1,7 +1,16 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    repeat(lines) { i ->
+        repeat(i) { print(' ')}
+
+        repeat(2 * (lines - i) - 1){ j ->
+            print(
+                if(j%2==0) char1 else char2
+            )
+        }
+        println()
+    }
 }
 
 fun main() {
