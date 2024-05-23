8777c4b3f48d0dce276bf8711d32f9bd237049a9
merajcode5 <mehrajmohd00@gmail.com>
2024-05-23 10:29:28 +0530

    Pattern1

--- ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/Pattern1/src/Task.kt
@@ -1,7 +1,18 @@
 import com.faangx.ktp.patterns.single.PatternMiniApp
 
 fun printPattern(lines: Int, char1: Char, char2: Char) {
-    TODO()
+    for (i in 0 until lines) {
+        for (j in 0 until lines) {
+            if (i == j) {
+                print(char1)
+            } else {
+                print(char2)
+            }
+        }
+        println()
+    }
+
+
 }
 
 fun main() {
