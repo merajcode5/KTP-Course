50f6dd58a1c57a96e43064105cec20a29198d6a6
merajcode5 <mehrajmohd00@gmail.com>
2024-05-29 18:00:04 +0530

    binomial

--- ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
+++ ProgrammingFundamentals/Ep3.Ext/BinomialExpansion/src/Task.kt
@@ -1,19 +1,66 @@
 import com.faangx.ktp.ext.BinomialExpansionMiniApp
 
 fun printBinomialExpansion(n: Int) {
-    TODO()
+
+    fun factorialOf(x: Int): Int {
+        return if (x <= 1) 1 else x * factorialOf(x - 1)
+    }
+    // nPr
+    fun permutationsOf(n: Int, r: Int): Int {
+        return factorialOf(n) / factorialOf(n - r)
+    }
+
+    // nCr
+    fun combinationsOf(n: Int, r: Int): Int {
+        return permutationsOf(n, r) / factorialOf(r)
+    }
+
+    // Binomial expansion
+    val result = StringBuilder()
+    for (k in 0..n) {
+        val coefficient = combinationsOf(n, k)
+        val aExponent = n - k
+        val bExponent = k
+
+        if (coefficient != 1) {
+            result.append("$coefficient")
+        }
+
+        if (aExponent > 0) {
+            result.append("a")
+            if (aExponent > 1) {
+                result.append("^$aExponent")
+            }
+        }
+
+        if (bExponent > 0) {
+            if (aExponent > 0) {
+                result.append(" * ")
+            }
+            result.append("b")
+            if (bExponent > 1) {
+                result.append("^$bExponent")
+            }
+        }
+
+        if (k < n) {
+            result.append(" + ")
+        }
+    }
+
+    println(result.toString())
 }
 
 fun factorialOf(x: Int): Int {
-    TODO()
+    return if (x <= 1) 1 else x * factorialOf(x - 1)
 }
 
 fun permutationsOf(n: Int, r: Int): Int {
-    TODO()
+    return factorialOf(n) / factorialOf(n - r)
 }
 
 fun combinationsOf(n: Int, r: Int): Int {
-    TODO()
+    return permutationsOf(n, r) / factorialOf(r)
 }
 
 fun main() {
